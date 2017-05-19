dd44f623bc2e8be29510279c03542288bfb2bfe3
ZomGod <starlingjavierc@hotmail.com>
2017-05-19 11:44:45 -0400

    ADD new

---
+++ src/main/java/alphaplus/IA/Main.java
@@ -0,0 +1,13 @@
+package alphaplus.IA;
+
+import java.net.SocketPermission;
+
+/**
+ * Created by No. GP62 on 19/05/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
+}
